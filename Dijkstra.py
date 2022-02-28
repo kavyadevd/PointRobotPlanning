832eff451265b0e3a3debc67f9675a6007f9a688
Kavyashree <kavya27.devadiga@gmail.com>
2022-02-28 00:36:04 -0500

    Validate input coordinates

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,4 +1,8 @@
 import pygame
+import numpy as np
+
+
+bot_clearance = 5
 
 pygame.init()
 screen_size = width, height = 400, 250
@@ -8,17 +12,26 @@ white = [255, 255, 255]
 screen.fill(white)
 
 
+def validate(pos):
+    if bot_clearance <= pos[0] >= (width-bot_clearance) or bot_clearance <= pos[1] >= (height-bot_clearance):
+        print('Entered ponit is not within map dimensions')
+
+
 def draw_map():
-    while True:
-        # circle(surface, color, center, radius)
-        pygame.draw.circle(screen, black, (300, (250-185)), 40)
-
-        # polygon(surface, color, points)
-        pygame.draw.polygon(
-            screen, black, [(115, 40), (75, 70), (105, 150), (36, 64)], True)
-
-        # Hexagon
-        pygame.display.flip()
+    # circle(surface, color, center, radius)
+    pygame.draw.circle(screen, black, (300, (250-185)), 40)
+    # polygon(surface, color, points)
+    pygame.draw.polygon(
+        screen, black, [(115, 40), (75, 70), (105, 150), (36, 64)], True)
+    # Hexagon
+    pygame.display.flip()
 
 
 draw_map()
+
+start_pos = [10, 250-10]
+validate(start_pos)
+goal_pos = [390, 250-245]
+validate(start_pos)    
+pygame.time.wait(5000)
+
