7217973743af002dcc5f53591c882587144fc0d4
Kavyashree <kavya27.devadiga@gmail.com>
2022-02-28 21:59:26 -0500

    Update validation of start goal points

--- Obstacle.py
+++ Obstacle.py
@@ -1,14 +1,20 @@
 import matplotlib.path as mplPath
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 
 class Obstacle:
     def __init__(self, clearance, point) -> None:
         self.clearance = clearance
         self.robot_pos = point
+        self.obstacles = []
 
     def AddObstacle(self, shape, points):
-        self.Obstacles.append(shape, points)
+        temp = []
+        temp.append(shape)
+        temp.append(points)
+        self.obstacles.append(temp)
         if shape == 'circle':
             self.circle_center = points[0]
             self.circle_radius = points[1][0]
@@ -20,13 +26,15 @@ class Obstacle:
         return True
 
     def InsidePolygon(self, point, polygon_points) -> bool:
-        polygon_shape = mplPath.Path(np.array([polygon_points]))
+        polygon_shape = mplPath.Path(np.array(polygon_points))
         robot_pos = (point[0], point[1])
-        return polygon_shape.contains_point(robot_pos)
+        flag = polygon_shape.contains_point(robot_pos)
+        return flag
 
     # Returns true if point in inside circle
     def InsideCircle(self, point) -> bool:
         # (x-x1)^2 + (y-y1)^2 <= (r+clearance)^2
         center = self.circle_center
         radius = self.circle_radius
-        return (point[0]-center[0])**2 + (point[1]-center[1])**2 <= (radius+self.clearance)**2
+        flag = ((point[0]-center[0])**2 + (point[1]-center[1])**2 <= (radius+self.clearance)**2)
+        return flag
