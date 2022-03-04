12d673ff5ec98848f3f401f8c9be60b47f496eab
Kavyashree <kavya27.devadiga@gmail.com>
2022-03-03 20:18:59 -0500

    Fix incorrect cost function

--- Node.py
+++ Node.py
@@ -1,12 +1,15 @@
-
 class Node:
-    def __init__(self, puzzle, i, parent,moves):
+    def __init__(self,game_config, i, parent,moves):
         self.parent = parent
         self.index = i
-        self.map_ = puzzle
+        self.gameparams = game_config
+        #self.map_ = puzzle
 
         # set weight accoring to ReadMe Fig 2
-        self.cost = 1 if  moves and (len(moves) == 1) else 1.4
+        if parent:
+            self.cost = (self.parent.cost+1) if  moves and (len(moves) == 1) else (self.parent.cost+1.4)
+        else :
+            self.cost = (1) if  moves and (len(moves) == 1) else (1.4)
         if not moves:
             self.cost = 0
-        self.moves = moves
+        self.moves = moves     
