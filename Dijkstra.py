0a884849608e8567345e864740ed81d8459fabe0
Kavyashree <kavya27.devadiga@gmail.com>
2022-03-04 19:42:38 -0500

    Visualise output points and path

--- Dijkstra.py
+++ Dijkstra.py
@@ -40,6 +40,75 @@ if not goal_pos:
 
 fig1, axes = plot.subplots()
 
+def PlotPath(path):
+    xs, ys = zip(*path)
+    plot.plot(xs,ys)
+
+def DrawMap(points):
+    # plot.subplots(121)
+    plot.ylim([0, 250])
+    plot.xlim([0, 400])
+
+    #Polygon 1
+    poly = polygon1_points
+    poly.append(poly[0])
+    xs, ys = zip(*poly)
+    plot.plot(xs,ys)
+    plot.fill(xs, ys, "c")
+
+    # Plot start goal points
+    plot.text(start_pos[0],start_pos[1],'Start')
+    plot.text(goal_pos[0],goal_pos[1],'Goal')
+    plot.plot(start_pos[0],start_pos[1], 'ro')
+    plot.plot(goal_pos[0],goal_pos[1], 'ro')
+
+    #Circle
+    Drawing_colored_circle = plot.Circle(( 300 , 185 ), 40 )
+    axes.add_artist( Drawing_colored_circle ) 
+
+    #Hexagon
+    poly = polygon2_points
+    poly.append(poly[0])
+    xs, ys = zip(*poly)
+    plot.plot(xs,ys)
+    plot.fill(xs, ys, "c")
+
+    # Clearance
+    poly = poly1_offset
+    xs, ys = zip(*poly)
+    plot.plot(xs,ys)
+
+    poly = hexa_offset
+    xs, ys = zip(*poly)
+    plot.plot(xs,ys)
+
+
+    circle_border = plot.Circle((300, 185), 45, color='r')
+    axes.add_patch(circle_border)
+
+    # Animate Dijkstra space
+    poly = polygon2_points
+    poly.append(poly[0])
+    xs, ys = zip(*poly)
+    global dataSet
+    plot.plot(xs,ys)
+    plot.fill(xs, ys, "c")
+
+    xs = [p[0] for p in points]
+    ys = [p[1] for p in points]
+
+    dataSet = np.array([xs, ys])
+    
+    line_ani = animation.FuncAnimation(fig1, animate_func, interval=1,   
+                                   frames=len(points)//20)
+    #line_ani.save('points.gif', writer='abc', fps=30)
+    plot.show()
+    line_ani.save('animation.gif', writer='PillowWriter', fps=30)
+
+def animate_func(num):
+    axes.plot(dataSet[0, :num+1], dataSet[1, :num+1])
+    axes.scatter(dataSet[0, num], dataSet[1, num], marker='o')
+
 # Create Obstacles
 obstacles_ = Obstacle(bot_clearance, start_pos, height, width,polygon1_points, polygon2_points)
 obstacles_.AddObstacle("circle", [[300, 185], [40]])
@@ -52,7 +121,8 @@ def StartGame(start_pos,goal_pos,bot_clearance):
     global obstacles_
     game_ = Game(start_pos, goal_pos, bot_clearance, obstacles_)
     path, points = game_.Start()
- 
+    DrawMap(points)
+    PlotPath(path)
     plot.show()
     #pygame.time.wait(5000)
 
